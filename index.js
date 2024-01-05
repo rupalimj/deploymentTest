55181aba3954f430931fefbe63e988ad88ce9dad
Rupali Jadhav <rupali1.jadhav@ril.com>
2024-01-05 11:59:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('rupalijadhav')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Please Login</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>Chai Aur Code</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`app listning on port: ${port}`)
+})
